930264664109033c894ad0801949a6f3cc5d432c
qz-tel <lx.wang@oscg.biz>
2023-07-13 11:22:06 +0800

    包裹邮件发送功能

--- web_zhaogu_advance/models/shipping_bill.py
+++ web_zhaogu_advance/models/shipping_bill.py
@@ -63,58 +63,14 @@ class ShippingBill(models.Model):
                 invoice_id.invoice_line_ids.filtered(lambda l: 'Wallet' in l.name).mapped('price_subtotal'))
             fee = sum(invoice_id.mapped('amount_total')) + (point_price or 0)
             openid = self.sale_partner_id.user_ids.wx_openid
-            if openid:
-                # 获取token
-                token = selfs.env['ir.config_parameter'].sudo().search([('key', '=', 'wechat.access_token')]).value
-                tmpl_id = "nyb0HsFu4oVOyR712tQFurlpt27foVsRwIb9pDge3vA",
-                tmpl_data = {
-                    "first": {
-                        "value": "您的订单已到仓:",
-                        "color": "#173177"
-                    },
-                    "orderno": {
-                        "value": self.name,
-                        "color": "#173177"
-                    },
-                    "amount": {
-                        "value": "%s%s" % (self.currency_id.symbol, '{0:,.2f}'.format(fee)),
-                        "color": "#173177"
-                    },
-                    "remark": {
-                        "value": "取件码[%s]" % self.picking_code,
-                        "color": "#173177"
-                    },
-                }
-                self.wx_information_send(token, openid, tmpl_data, tmpl_id)
-
-            # 发送邮件
-            self.env.ref('shipping_bills.mail_template_data_shipping_bill_to_warehouse').send_mail(self.id)
-
-            # 发送短信
-            if self.sale_partner_id.phone:
-                msg = 'Package [%s] has arrived at warehouse. Shipment Cost is [%s].' \
-                      'Please make payment via your registered account.' \
-                      'For queries, contact our customer service. [%s]' % (self.tracking_no, fee, self.sale_partner_id.company_id.name)
-                self.send_message_post(msg)
-
-        return result
-
-    def multi_action_change(selfs):
-        result = super().multi_action_change()
-        for self in selfs:
-            invoice_id = self.sale_invoice_ids.filtered(
-                lambda l: l.payment_state not in ['paid', 'reversed', 'invoicing_legacy'] and l.state != 'cancel')
-            point_price = -sum(
-                invoice_id.invoice_line_ids.filtered(lambda l: 'Wallet' in l.name).mapped('price_subtotal'))
-            fee = sum(invoice_id.mapped('amount_total')) + (point_price or 0)
-            openid = self.sale_partner_id.user_ids.wx_openid
-            if openid:
-                # 获取token
-                token = selfs.env['ir.config_parameter'].sudo().search([('key', '=', 'wechat.access_token')]).value
-                tmpl_id = "nyb0HsFu4oVOyR712tQFurlpt27foVsRwIb9pDge3vA",
-                tmpl_data = {
-                    "first": {
-                            "value": "您的订单已改泡:",
+            if not self.has_changed:
+                if openid:
+                    # 获取token
+                    token = selfs.env['ir.config_parameter'].sudo().search([('key', '=', 'wechat.access_token')]).value
+                    tmpl_id = "nyb0HsFu4oVOyR712tQFurlpt27foVsRwIb9pDge3vA",
+                    tmpl_data = {
+                        "first": {
+                            "value": "您的订单已到仓:",
                             "color": "#173177"
                         },
                         "orderno": {
@@ -129,17 +85,63 @@ class ShippingBill(models.Model):
                             "value": "取件码[%s]" % self.picking_code,
                             "color": "#173177"
                         },
-                }
-                self.wx_information_send(token, openid, tmpl_data, tmpl_id)
+                    }
+                    self.wx_information_send(token, openid, tmpl_data, tmpl_id)
 
-            # 发送邮件
-            self.env.ref('shipping_bills.mail_template_data_shipping_bill_modified_foam').send_mail(self.id)
+                # 发送邮件
+                self.env.ref('shipping_bills.mail_template_data_shipping_bill_to_warehouse').send_mail(self.id)
 
-            # 发送短信
-            if self.sale_partner_id.phone:
-                msg = 'Package [%s] has been repackaged. New shipment cost is [%s].' \
-                      'Please adjust your payment. For any concerns, ' \
-                      'contact our customer service. [%s]' % (self.tracking_no, fee, self.sale_partner_id.company_id.name)
-                self.send_message_post(msg)
+                # 发送短信
+                if self.sale_partner_id.phone:
+                    msg = 'Package [%s] has arrived at warehouse. Shipment Cost is [%s].' \
+                          'Please make payment via your registered account.' \
+                          'For queries, contact our customer service. [%s]' % (self.tracking_no, fee, self.sale_partner_id.company_id.name)
+                    self.send_message_post(msg)
+
+        return result
+
+    def multi_action_change(selfs):
+        result = super().multi_action_change()
+        for self in selfs:
+            invoice_id = self.sale_invoice_ids.filtered(
+                lambda l: l.payment_state not in ['paid', 'reversed', 'invoicing_legacy'] and l.state != 'cancel')
+            point_price = -sum(
+                invoice_id.invoice_line_ids.filtered(lambda l: 'Wallet' in l.name).mapped('price_subtotal'))
+            fee = sum(invoice_id.mapped('amount_total')) + (point_price or 0)
+            openid = self.sale_partner_id.user_ids.wx_openid
+            if self.has_changed:
+                if openid:
+                    # 获取token
+                    token = selfs.env['ir.config_parameter'].sudo().search([('key', '=', 'wechat.access_token')]).value
+                    tmpl_id = "nyb0HsFu4oVOyR712tQFurlpt27foVsRwIb9pDge3vA",
+                    tmpl_data = {
+                        "first": {
+                                "value": "您的订单已改泡:",
+                                "color": "#173177"
+                            },
+                            "orderno": {
+                                "value": self.name,
+                                "color": "#173177"
+                            },
+                            "amount": {
+                                "value": "%s%s" % (self.currency_id.symbol, '{0:,.2f}'.format(fee)),
+                                "color": "#173177"
+                            },
+                            "remark": {
+                                "value": "取件码[%s]" % self.picking_code,
+                                "color": "#173177"
+                            },
+                    }
+                    self.wx_information_send(token, openid, tmpl_data, tmpl_id)
+
+                # 发送邮件
+                self.env.ref('shipping_bills.mail_template_data_shipping_bill_modified_foam').send_mail(self.id)
+
+                # 发送短信
+                if self.sale_partner_id.phone:
+                    msg = 'Package [%s] has been repackaged. New shipment cost is [%s].' \
+                          'Please adjust your payment. For any concerns, ' \
+                          'contact our customer service. [%s]' % (self.tracking_no, fee, self.sale_partner_id.company_id.name)
+                    self.send_message_post(msg)
         return result
 
