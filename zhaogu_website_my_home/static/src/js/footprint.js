4e4b78975b99afcaa2f0aef15d3159822a6be9e8
qz-tel <lx.wang@oscg.biz>
2023-10-26 15:41:09 +0800

    1026需求优化

--- zhaogu_website_my_home/static/src/js/footprint.js
+++ zhaogu_website_my_home/static/src/js/footprint.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.star').on('click', function () {
       var selectedStar = $(this).data('star');
       $('.star').removeClass('active');
-      for (var i = 1; i &lt;= selectedStar; i++) {
+      for (var i = 1; i <= selectedStar; i++) {
         $('.star[data-star=' + i + ']').addClass('active');
       }
       $('#ratingText').val(selectedStar);
