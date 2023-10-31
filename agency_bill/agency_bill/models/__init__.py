ef80115de01fe0f41ece48da75f2d402c6fe8809
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-10-31 14:00:15 +0800

    Delete agency_bill/agency_bill directory

--- agency_bill/agency_bill/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-from . import res_partner
-from . import account_move
-from . import account_move_line
-from . import stock_picking
-from . import stock_move
-from . import website
