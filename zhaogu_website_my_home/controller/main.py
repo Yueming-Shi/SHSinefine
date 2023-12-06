cc36b5bce9790415c30678d782dda6ca2ce4c79a
qz-tel <lx.wang@oscg.biz>
2023-12-06 13:47:50 +0800

    12/6日需求修改

--- zhaogu_website_my_home/controller/main.py
+++ zhaogu_website_my_home/controller/main.py
@@ -31,7 +31,7 @@ class Controller(http.Controller):
         if request.website.is_display_personal_home:
             request.redirect('/my/personal/home')
         else:
-            return super(Controller, self).home(**kw)
+            return super().home()
 
     @http.route('/my/personal/home', type='http', auth='public', methods=['GET'], website=True)
     def sale_fill_order_create_view(self):
