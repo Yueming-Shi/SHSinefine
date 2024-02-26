a561412500c340e19da69cd5e92e4bab5d6d2657
qz-tel <lx.wang@oscg.biz>
2024-02-26 12:53:34 +0800

    2/22日需求修改

--- shipping_bills/models/shipping_large_parcel.py
+++ shipping_bills/models/shipping_large_parcel.py
@@ -212,7 +212,7 @@ class ShippingLargeParcel(models.Model):
             fee = first_total_price + (
                     weight - first_weight) / next_weight_to_ceil * next_price_unit
         for shipping in merge_shippings:
-            alone_fee = shipping.actual_weight / size_weight * fee
+            alone_fee = shipping.actual_weight / sum(merge_shippings.mapped('actual_weight')) * fee
             shipping.write({'fee': alone_fee, 'currency_id': shipping_factor.currency_id.id, 'size_weight': shipping.actual_weight, })
             shipping.vvip_action_remind_payment(alone_fee)
         return merge_shippings
