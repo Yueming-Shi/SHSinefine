fb1609426fc4e4736b6c901d38c22bb051b6f11f
qz-tel <lx.wang@oscg.biz>
2024-02-27 12:55:49 +0800

    2/22日需求修改

--- shipping_bills/models/shipping_large_parcel.py
+++ shipping_bills/models/shipping_large_parcel.py
@@ -208,8 +208,7 @@ class ShippingLargeParcel(models.Model):
                 next_price_unit = shipping_factor.next_price_unit
                 next_weight_to_ceil = shipping_factor.next_weight_to_ceil
 
-        size_weight = sum(merge_shippings.mapped('size_weight'))
-
+        size_weight = round(sum(merge_shippings.mapped('size_weight')), 2)
 
         weight = math.ceil(
             size_weight * 1000 / next_weight_to_ceil) * next_weight_to_ceil
@@ -219,7 +218,6 @@ class ShippingLargeParcel(models.Model):
         else:
             fee = first_total_price + (
                     weight - first_weight) / next_weight_to_ceil * next_price_unit
-        raise UserError('%s, %s, %s, %s, %s, %s, %s' % (first_weight, first_total_price, next_price_unit, next_weight_to_ceil, size_weight, fee, weight))
         for shipping2 in merge_shippings:
             alone_fee = shipping2.size_weight / sum(merge_shippings.mapped('size_weight')) * fee
             shipping2.write({'fee': alone_fee, 'currency_id': shipping_factor.currency_id.id})
